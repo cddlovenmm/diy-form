cd25a7710e8050aefa8e08e063ac3d27b50f9da8
高大申 <milliongds@163.com>
2023-05-04 13:37:10 +0800

    更改数据库文件

--- updates/create_diy_form_table.php
+++ updates/create_diy_form_table.php
@@ -23,6 +23,45 @@ class CreateDiyFormTable extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        Schema::create('diy_form_redirect_uri', function (Blueprint $table) {
+            $table->id();
+            $table->string('uri')->default('')->nullable(false)->default('跳转地址');
+            $table->timestamps();
+        });
+
+        Schema::create('diy_form_info', function (Blueprint $table) {
+            $table->id();
+            $table->integer('form_id')->nullable(false)->default(0)->comment('表单id');
+            $table->string('channel')->nullable(false)->default('')->comment('渠道');
+            $table->string('images')->nullable(false)->default('')->comment('图片');
+            $table->string('admin_name')->nullable(false)->default('')->comment('管理员姓名');
+            $table->string('admin_avater')->nullable(false)->default('')->comment('管理员头像');
+            $table->string('admin_job')->nullable(false)->default('')->comment('管理员职位');
+            $table->string('admin_message')->nullable(false)->default('')->comment('管理员信息');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('diy_form_data', function (Blueprint $table) {
+            $table->id();
+            $table->integer('form_id')->nullable(false)->default(0)->comment('表单id');
+            $table->text('data')->comment('表单数据');
+            $table->timestamps();
+
+            $table->index(['form_id'], 'IDX_FID');
+        });
+        
+        Schema::create('diy_form_data_count', function (Blueprint $table) {
+            $table->id();
+            $table->integer('form_id')->nullable(false)->default(0)->comment('表单id');
+            $table->string('channel')->nullable(false)->default('')->comment('渠道');
+            $table->integer('data_count')->nullable(false)->default(0)->comment('信息数量');
+            $table->timestamps();
+
+            $table->index(['channel'], 'IDX_CHANNEL');
+        });
     }
 
     /**
@@ -33,5 +72,9 @@ class CreateDiyFormTable extends Migration
     public function down()
     {
         Schema::dropIfExists('diy_form');
+        Schema::dropIfExists('diy_form_data_count');
+        Schema::dropIfExists('diy_form_data');
+        Schema::dropIfExists('diy_form_info');
+        Schema::dropIfExists('diy_form_redirect_uri');
     }
 }
